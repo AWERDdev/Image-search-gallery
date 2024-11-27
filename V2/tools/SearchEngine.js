698fa970139ffe99aa179bdf98777f1087d8bd9d
AWERDdev <kokorami565@gmail.com>
2024-11-27 18:24:05 +0200

    In work

--- V2/tools/SearchEngine.js
+++ V2/tools/SearchEngine.js
@@ -1,14 +1,24 @@
+
 const url = ' https://api.unsplash.com/search/photos';
 const APIKEY = 'q-Q28KB-Tm1tSvJ-copIMQOGxagnyLiazs6gf0mZMO4';
 const search_btn = document.getElementById('search-btn');
 search_btn.addEventListener('click',customiseURL)
-async function  customiseURL(){
-    try{
-        const searchInput = document.getElementById('search-bar').value.trim();
-        console.log(searchInput)
+let newURL;
 
-    }catch(error){
-        console.error(error)
-
+//* function that will set the url needed to fetch the data from the API
+ function  customiseURL(){
+    //* get search query
+    const searchInput = document.getElementById('search-bar').value.trim();
+    if(!searchInput){
+console.log('please enter a value')
+    }else{
+        let newURL = `${url}?query=${searchInput}&client_id=${APIKEY}`
+        console.log(searchInput)//test
+        console.log(newURL)
     }
+
+ 
+}
+function fetchData(){
+
 }
